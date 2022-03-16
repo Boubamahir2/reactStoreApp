15a4649ed5fd30bec1a690428be0ae14cb55e607
Boubamahir2 <amamahir1@gmail.com>
2022-03-16 20:45:39 +0100

    cart functionality added

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,10 +8,19 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+//check local storage first
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
-  total_amounts: 0,
+  total_amount: 0,
   shipping_fee: 351,
 };
 
@@ -23,8 +32,27 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
+  //remove item
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
+  //toggle amount
+  const toggleAmount = (id, value) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  //useEffect to save products in the local storage
+  useEffect(() => {
+    dispatch({ type: COUNT_CART_TOTALS });
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -9,7 +9,86 @@ import {
 const cart_reducer = (state, action) => {
   if (action.type === ADD_TO_CART) {
     const { id, color, amount, product } = action.payload;
-    return { ...state };
+    //return the item that whose id and color matches the state item
+    const tempItem = state.cart.find((i) => i.id === id + color);
+    if (tempItem) {
+      //increase  item amount if it already exist inside the cart
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          //we have to check for the stock as well
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
+    } else {
+      //add item from scratch
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+  //delete items from cart
+  if (action.type === REMOVE_CART_ITEM) {
+    const tempCart = state.cart.filter((cart) => cart.id !== action.payload);
+    return { ...state, cart: tempCart };
+  }
+  //clear the cart
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  //toggle functionality
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
+    const tempCart = state.cart.map((item) => {
+      if (item.id === id) {
+        if (value === "inc") {
+          let newAmount = item.amount + 1;
+          if (newAmount > item.max) {
+            newAmount = item.max;
+          }
+          return { ...item, amount: newAmount };
+        }
+        if (value === "dec") {
+          let newAmount = item.amount - 1;
+          if (newAmount < 1) {
+            newAmount = 1;
+          }
+          return { ...item, amount: newAmount };
+        }
+      } else {
+        return item;
+      }
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === COUNT_CART_TOTALS) {
+    const { total_items, total_amount } = state.cart.reduce(
+      (total, cartItem) => {
+        const { price, amount } = cartItem;
+        //calculate the total in cart
+        total.total_items += amount;
+        total.total_amount += price * amount;
+        return total;
+      },
+      {
+        total_items: 0,
+        total_amount: 0,
+      }
+    );
+    return { ...state, total_items: total_items, total_amount: total_amount };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
